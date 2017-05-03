a3d2390321c5ffb494444b696f60b20a9179c07b
TheophileWalter <theophilewalter@gmail.com>
2017-05-03 10:25:42 +0200

    Added some sprites, improved render.ml

--- render.ml
+++ render.ml
@@ -24,27 +24,39 @@ let bubble_sprite =
 	image_from_ppm "sprites/bubble.ppm"
 let star_sprite =
 	image_from_ppm "sprites/star.ppm"
-let monster_sprite =
-	image_from_ppm "sprites/monster.ppm"
+let smonster_sprite =
+	image_from_ppm "sprites/smonster.ppm"
+let dmonster_sprite =
+	image_from_ppm "sprites/dmonster.ppm"
+let wall_sprite =
+	image_from_ppm "sprites/wall.ppm"
+let attractor_sprite =
+	image_from_ppm "sprites/attractor.ppm"
 let unknow_sprite =
 	image_from_ppm "sprites/unknow.ppm"
 
 (* Simple function to draw a single element *)
 let draw_single_element element =
 	match element with
-	| GravField((posX, posY))      -> draw_image unknow_sprite  (int_of_float posX) (int_of_float posY) (* Sprite to do *)
-	| Star((posX, posY))           -> draw_image star_sprite    (int_of_float posX) (int_of_float posY)
-	| Bubble((posX, posY))         -> draw_image bubble_sprite  (int_of_float posX) (int_of_float posY)
-	| Attractor((posX, posY), _)   -> draw_image unknow_sprite  (int_of_float posX) (int_of_float posY) (* Sprite to do *)
-	| Rope((posX, posY))           -> () (* Rope is not a simple sprite *)
-	| Wall((posX, posY), _)        -> draw_image unknow_sprite  (int_of_float posX) (int_of_float posY) (* Sprite to do *)
-	| SMonster((posX, posY), _)    -> draw_image monster_sprite (int_of_float posX) (int_of_float posY)
-	| Player((posX, posY), _, _)   -> draw_image player_sprite  (int_of_float posX) (int_of_float posY)
-	| DMonster((posX, posY), _, _) -> draw_image monster_sprite (int_of_float posX) (int_of_float posY) (* Sprite to do *)
+	| GravField((posX, posY))      -> () (* No sprite *)
+	| Star((posX, posY))           -> draw_image star_sprite      (int_of_float posX) (int_of_float posY)
+	| Bubble((posX, posY))         -> draw_image bubble_sprite    (int_of_float posX) (int_of_float posY)
+	| Attractor((posX, posY), _)   -> draw_image attractor_sprite (int_of_float posX) (int_of_float posY)
+	| Wall((posX, posY), _)        -> draw_image wall_sprite      (int_of_float posX) (int_of_float posY)
+	| SMonster((posX, posY), _)    -> draw_image smonster_sprite  (int_of_float posX) (int_of_float posY)
+	| Player((posX, posY), _, _)   -> draw_image player_sprite    (int_of_float posX) (int_of_float posY)
+	| DMonster((posX, posY), _, _) -> draw_image dmonster_sprite  (int_of_float posX) (int_of_float posY)
+	(*| _((posX, posY))              -> draw_image unknow_sprite    (int_of_float posX) (int_of_float posY)
+	| _((posX, posY), _)           -> draw_image unknow_sprite    (int_of_float posX) (int_of_float posY)
+	| _((posX, posY), _, _)        -> draw_image unknow_sprite    (int_of_float posX) (int_of_float posY)*)
 
 
-(* Just call this function to draw the level *)
-let draw_level level =
+(* Just call this function to draw the level
+ * @params
+ *   level of context : the level
+ *   sync  of bool    : true to synchronize, false to don't
+ *)
+let draw_level level sync =
 	(* Clear old drawing *)
 	clear_graph();
 	(* Draw background *)
@@ -56,4 +68,4 @@ let draw_level level =
 		| t::q -> draw_single_element t; draw_elements q;
 	in
 	draw_elements level;
-	synchronize()
+	if sync then synchronize()
