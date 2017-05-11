4b7afc3b6bac9464c4b2d80da58348b1fb725423
TheophileWalter <theophilewalter@gmail.com>
2017-05-11 13:04:29 +0200

    Corrected bug on cutting too length ropes

--- game.ml
+++ game.ml
@@ -3,14 +3,18 @@ open Basephysics
 open Gamemechanics
 
 (* Load a file if there is one given as a parameter *)
-let level =
+let levelInfo =
 	(*Printf.printf "Loading level\n%!";*)
 	if (Array.length Sys.argv) >= 2 then
-		loadLevel Sys.argv.(1)
+		(-1, loadLevel Sys.argv.(1))
 	else
-		failwith "You must specify a level file!"
+		(1, loadLevel "levels/1.lvl")
 
-let () = 
+let () =
 	try
-		game_loop level
+		try
+			game_loop (snd levelInfo)
+		with
+		| Gametypes.EndGame(Win) -> Printf.printf "Gagné !\n";
+		| Gametypes.EndGame(Die) -> Printf.printf "Perdu !\n"
 	with Graphics.Graphic_failure(_) -> exit 0
